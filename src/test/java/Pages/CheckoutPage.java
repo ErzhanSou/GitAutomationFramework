e3cbbe6a869eaa8f356c1750e1345325c367f6ab
Ayasue <suinalieva.aziza@gmail.com>
2024-05-02 11:22:53 -0500

    added checkoutPage and chechoutTests

---
+++ src/test/java/Pages/CheckoutPage.java
@@ -0,0 +1,9 @@
+package Pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CheckoutPage {
+    @FindBy(id = "some_id")
+    public WebElement checkoutButton;
+}
