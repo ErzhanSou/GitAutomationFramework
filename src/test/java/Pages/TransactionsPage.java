60f09408c817c9e7774376da5759f2a141075afc
abylkasymovE <emirabylkasymov@gmail,com>
2024-05-02 11:22:39 -0500

    added transactions pages

---
+++ src/test/java/Pages/TransactionsPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
+
+public class TransactionsPage {
+
+
+
+
+    @FindBy(id = "id")
+    public WebElement amount;
+}
